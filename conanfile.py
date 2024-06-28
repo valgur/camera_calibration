b09e492be1c14107ef210ce95f539c782ac3dbc9
Martin Valgur <martin.valgur@gmail.com>
2024-06-28 18:10:47 +0300

    Add option to use system Qt5

--- conanfile.py
+++ conanfile.py
@@ -1,15 +1,25 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps, CMake
 from conan.tools.env import VirtualBuildEnv
+from conan.tools.system.package_manager import Apt
 
 
 class PuzzlepaintCameraCalibrationPackage(ConanFile):
     name = "puzzlepaint-camera_calibration"
     version = "0.1"
     settings = "os", "compiler", "build_type", "arch"
-    default_options = {
-        "*/*:shared": False,
+    options = {
+        "system_qt5": [True, False],
     }
+    default_options = {
+        "system_qt5": True,
+        "*/*:shared": False,
+        "qt/*:shared": True,  # must be shared for plugins to work
+    }
+
+    def config_options(self):
+        if self.settings.os != "Linux":
+            del self.options.system_qt5
 
     def layout(self):
         cmake_layout(self)
@@ -19,19 +29,26 @@ class PuzzlepaintCameraCalibrationPackage(ConanFile):
         self.requires("boost/1.85.0")
         self.requires("eigen/3.4.0")
         self.requires("glew/2.2.0")
-        self.requires("libpng/[>=1.6 <2]")
+        self.requires("libpng/[>=1.6 <2]", force=True)
         self.requires("loguru/cci.20230406", options={"enable_streams": True, "replace_glog": True})
-        # self.requires("opencv/4.9.0")
         self.requires("opengl/system")
         self.requires("opengv/cci.20200806")
         self.requires("rapidjson/cci.20230929")
-        self.requires("qt/5.15.14", options={"qtx11extras": True})
         self.requires("sophus/1.22.10")
         self.requires("tinyxml2/10.0.0")
         self.requires("vulkan-loader/1.3.268.0")
         self.requires("xorg/system")
         self.requires("yaml-cpp/0.8.0")
         self.requires("zlib/1.3.1")
+        # self.requires("opencv/4.9.0")
+
+        if not self.options.get_safe("system_qt5"):
+            self.requires("qt/5.15.14", options={"essential_modules": False, "qtx11extras": True})
+
+    def system_requirements(self):
+        if self.options.get_safe("system_qt5"):
+            apt = Apt(self)
+            apt.install(["qtbase5-dev", "libqt5opengl5-dev", "libqt5x11extras5-dev"], check=True)
 
     def build_requirements(self):
         self.test_requires("gtest/1.14.0")
