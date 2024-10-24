d0a0f3ada29be4696b5bd0f8dc6dd45fa97ceff6
Martin Valgur <martin.valgur@gmail.com>
2024-10-24 13:26:30 +0300

    conanfile.py: use libv4l Conan package

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,7 @@
 from conan import ConanFile
 from conan.tools.cmake import cmake_layout, CMakeToolchain, CMakeDeps, CMake
 from conan.tools.env import VirtualBuildEnv, Environment
+from conan.tools.gnu import PkgConfigDeps
 from conan.tools.system.package_manager import Apt
 
 
@@ -10,57 +11,61 @@ class PuzzlepaintCameraCalibrationPackage(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "system_qt5": [True, False],
+        "system_xorg": [True, False],
     }
     default_options = {
         "system_qt5": True,
+        "system_xorg": True,
         "*/*:shared": False,
         "qt/*:shared": True,  # must be shared for plugins to work
     }
-
-    def config_options(self):
-        if self.settings.os != "Linux":
-            del self.options.system_qt5
 
     def layout(self):
         cmake_layout(self)
 
     def requirements(self):
-        self.requires("apriltag/3.3.0")
-        self.requires("boost/1.85.0")
         self.requires("eigen/3.4.0")
-        self.requires("glew/2.2.0")
-        self.requires("libpng/1.6.43", force=True)
-        self.requires("loguru/cci.20230406", options={"enable_streams": True, "replace_glog": True})
-        self.requires("opengl/system")
-        self.requires("opengv/cci.20200806")
-        self.requires("rapidjson/cci.20230929")
         self.requires("sophus/1.22.10")
-        self.requires("tinyxml2/10.0.0")
-        self.requires("vulkan-loader/1.3.268.0")
-        self.requires("xorg/system")
+        self.requires("apriltag/3.4.2")
+        self.requires("opengv/cci.20200806")
+        self.requires("libv4l/1.28.1")
+        self.requires("boost/1.86.0")
+        self.requires("loguru/cci.20230406", options={"enable_streams": True, "replace_glog": True})
+        self.requires("rapidjson/cci.20230929")
         self.requires("yaml-cpp/0.8.0")
-        self.requires("zlib/1.3.1")
-
-        if not self.options.get_safe("system_qt5"):
-            self.requires("qt/5.15.14", options={"essential_modules": False, "qtx11extras": True})
+        self.requires("tinyxml2/10.0.0")
+        self.requires("libpng/[>=1.6 <2]")
+        self.requires("glew/2.2.0")
+        self.requires("opengl/system")
+        self.requires("vulkan-loader/1.3.290.0")
+        if not self.options.system_qt5:
+            self.requires("qt/[~5.15]", options={"qtx11extras": True})
+        if self.options.system_xorg:
+            self.requires("xorg/system", force=True)
+        else:
+            self.requires("libx11/1.8.10", force=True)
 
     def system_requirements(self):
-        apt = Apt(self)
-        apt.install(["libv4l-dev"], check=True)
-        if self.options.get_safe("system_qt5"):
-            apt.install(["qtbase5-dev", "libqt5opengl5-dev", "libqt5x11extras5-dev"], check=True)
+        if self.options.system_qt5:
+            Apt(self).install(["qtbase5-dev", "libqt5opengl5-dev", "libqt5x11extras5-dev"], check=True)
 
     def build_requirements(self):
-        self.test_requires("gtest/1.14.0")
-        self.tool_requires("glslang/1.3.268.0")
+        if not self.conf.get("tools.gnu:pkg_config", default=False, check_type=str):
+            self.tool_requires("pkgconf/[>=2.2 <3]")
+        self.tool_requires("glslang/1.3.290.0")
+        self.test_requires("gtest/1.15.0")
 
     def generate(self):
+        VirtualBuildEnv(self).generate()
+
         tc = CMakeToolchain(self)
         tc.generate()
+
         deps = CMakeDeps(self)
         deps.generate()
-        venv = VirtualBuildEnv(self)
-        venv.generate()
+
+        deps = PkgConfigDeps(self)
+        deps.generate()
 
         if not self.options.get_safe("system_qt5") and self.settings.os in ["Linux", "FreeBSD"]:
             # Qt fails to find system fonts otherwise
